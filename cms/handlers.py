ecce092765eda95a77f2d007a37c5b3d6ecc232d
Sean Hoffman <sean-hoffman@pluralsight.com>
2019-11-25 14:08:25 -0700

    Task 1

--- cms/handlers.py
+++ cms/handlers.py
@@ -3,4 +3,10 @@ from flask import request, render_template
 
 from cms import app
 from cms.admin.models import Content, Type
+
+from logging import getLogger
+
+request_log = getLogger('werkeug');
+
+request_log.disabled = True
 #!
