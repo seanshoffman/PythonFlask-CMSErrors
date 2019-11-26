a2e752755886de5db98ec6c0a5455f2208bff60c
Sean Hoffman <sean-hoffman@pluralsight.com>
2019-11-25 20:56:21 -0700

    Task 2

--- cms/handlers.py
+++ cms/handlers.py
@@ -5,8 +5,13 @@ from cms import app
 from cms.admin.models import Content, Type
 
 from logging import getLogger
+#!
 
 request_log = getLogger('werkzeug')
 
 request_log.disabled = True
-#!
+
+def configure_logging(name, level):
+    log = getLogger(name)
+    log.setLevel(level)
+
